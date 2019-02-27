c36af09973d3f5501cec63ce5ade4c7e5ffe4e24
yanghaoran <1064137887@qq.com>
2019-02-27 17:03:30 +0800

    【简要描述】：二叉查找树 【详细描述】：完成二叉树查找树的编写和测试 【修 改 人】：杨浩然

---
+++ DataStruct/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include "BSTree.h"
+
+static int arr[] = { 1,5,4,3,2,6 };
+#define TBL_SIZE(a) ((sizeof(a)) / (sizeof(a[0])))
+
+int main()
+{
+	int i, ilen;
+	BSTree<int>* tree = new BSTree<int>();
+	
+	std::cout << "== 依次添加: ";
+	ilen = TBL_SIZE(arr);
+	for (i = 0; i < ilen; i++)
+	 {
+	     std::cout << arr[i] << " ";
+	     tree->Insert(arr[i]);
+	 }
+	
+	std::cout << "\n== 前序遍历: ";
+	tree->PreOrder();
+
+	std::cout << "\n== 中序遍历: ";
+	tree->InOrder();
+
+	std::cout << "\n== 后序遍历: ";
+	tree->PostOrder();
+	std::cout << std::endl;
+	//
+	std::cout << "== 最小值: " << tree->Minimum() << std::endl;
+	std::cout << "== 最大值: " << tree->Maximum() << std::endl;
+	std::cout << "== 树的详细信息: " << std::endl;
+	tree->Print();
+
+	std::cout << "\n== 删除根节点: " << arr[3];
+	tree->Remove(arr[3]);
+
+	std::cout << "\n== 中序遍历: ";
+	tree->InOrder();
+	std::cout << std::endl;
+	// 销毁二叉树
+	tree->Destroy();
+	system("pause");
+	return 0;
+}
